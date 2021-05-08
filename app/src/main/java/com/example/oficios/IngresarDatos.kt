d1b3d65ce6751efad664c609d478db8d653e67d1
Mariana <aoimashup@gmail.com>
2021-05-07 19:33:54 -0500

    Update a button in `IngresaDatos`
    - Remove unused import to Intent
    - Add references to button defined in layout
    - Add  `onClickListener` for button added
    - Update button id defined in layout
    - Update button label

--- app/src/main/java/com/example/oficios/IngresarDatos.kt
+++ app/src/main/java/com/example/oficios/IngresarDatos.kt
@@ -1,8 +1,8 @@
 package com.example.oficios
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class IngresarDatos : AppCompatActivity() {
@@ -10,6 +10,12 @@ class IngresarDatos : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ingresar_datos)
 
+        val confirmarDatosBtn = findViewById<Button>(R.id.confirmarDatos)
+
+        confirmarDatosBtn?.setOnClickListener()
+        {
+            Toast.makeText(this, "Confimo datos", Toast.LENGTH_LONG).show()
+        }
 
     }
 }
