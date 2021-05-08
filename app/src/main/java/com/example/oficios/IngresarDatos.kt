99bb7e080b2b7b483ff630afa527ce65c86872c4
Mariana <aoimashup@gmail.com>
2021-05-07 21:15:50 -0500

    Add `onClickListener` method into IngresarDatos activity
    - Move button behavior into new method
    - Remove deprecated behavior

--- app/src/main/java/com/example/oficios/IngresarDatos.kt
+++ app/src/main/java/com/example/oficios/IngresarDatos.kt
@@ -1,6 +1,7 @@
 package com.example.oficios
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -9,13 +10,15 @@ class IngresarDatos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ingresar_datos)
+    }
 
-        val confirmarDatosBtn = findViewById<Button>(R.id.confirmarDatos)
-
-        confirmarDatosBtn?.setOnClickListener()
-        {
-            Toast.makeText(this, "Confimo datos", Toast.LENGTH_LONG).show()
+    fun onClickListener(view: View) {
+        if (view is Button) {
+            when (view.id) {
+                R.id.confirmarDatos -> {
+                    Toast.makeText(this, "Confimo datos", Toast.LENGTH_LONG).show()
+                }
+            }
         }
-
     }
 }
